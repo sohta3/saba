319bcaa745b5e6029348f0c2efaadc3abb8f2f17
Sogo Ohta <sogo.ohta@eure.jp>
2025-07-24 10:49:10 +0900

    第3章 「テストの実行」まで

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -52,5 +52,25 @@ impl HttpClient {
                 return Err(Error::Network(format!("Failed to send a request to TCP stream".to_string(),))),
             },
         };
+
+        let mut received = Vec::new();
+        loop {
+            let mut buffer = [0u8; 4096];
+            let bytes_read = match stream.read(&mut buf) {
+                Ok(bytes) => bytes,
+                Err(_) => {
+                    return Err(Error::Network("Failed to receive a request from TCP stream".to_string(),))),
+                },
+            };
+            if bytes_read == 0 {
+                break;
+            }
+            received.extend_from_slice(&buffer[..bytes_read]);
+        }
+
+        match core::str::from_utf8(&received) {
+            Ok(response) => HttpResponse::new(response.to_string()),
+            Err(e) => Err(Error::Network(format!("Invalid received response:{}", e))),
+        }
     }
 }
